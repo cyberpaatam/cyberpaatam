bc63e514f6c8f8b62dc85a1e5ecbbfe6337c7efe
cyberpaatam <cyberpaatam@gmail.com>
2025-07-15 22:46:33 +0530

    main js

---
+++ firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAH14XuRimzgAnqC5ejuomCRlpUZm-eeok",
+    authDomain: "cyberpaatam-e266d.firebaseapp.com",
+    databaseURL: "https://cyberpaatam-e266d-default-rtdb.firebaseio.com",
+    projectId: "cyberpaatam-e266d",
+    storageBucket: "cyberpaatam-e266d.firebasestorage.app",
+    messagingSenderId: "583622641591",
+    appId: "1:583622641591:web:3b8ea77bd1327963eeca32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
